7c36b41ab36e6490e2e1db3f7accf34ebfb80f69
Pranit daphale <158754224+PranitWork@users.noreply.github.com>
2025-08-21 19:35:24 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import DataContext from "./context/DataContext.jsx"
+import DataContext from "./context/DataContext.jsx" 
 
 createRoot(document.getElementById("root")).render(
     <DataContext>
